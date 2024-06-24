e52af827eabfe50ec2d81449f329cefe93640e51
MAOYY <huacongchongchong@gmail.com>
2024-06-25 00:30:20 +0800

    更改模型

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -4,6 +4,7 @@ import redis from '../../utils/redis';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from './auth/[...nextauth]';
 
+
 type Data = string;
 interface ExtendedNextApiRequest extends NextApiRequest {
   body: {
@@ -14,10 +15,10 @@ interface ExtendedNextApiRequest extends NextApiRequest {
 // Create a new ratelimiter, that allows 2 requests per day
 const ratelimit = redis
   ? new Ratelimit({
-      redis: redis,
-      limiter: Ratelimit.fixedWindow(2, '1440 m'),
-      analytics: true,
-    })
+    redis: redis,
+    limiter: Ratelimit.fixedWindow(2, '1440 m'),
+    analytics: true,
+  })
   : undefined;
 
 export default async function handler(
@@ -53,6 +54,7 @@ export default async function handler(
     }
   }
 
+
   const imageUrl = req.body.imageUrl;
   // POST request to Replicate to start the image restoration generation process
   let startResponse = await fetch('https://api.replicate.com/v1/predictions', {
@@ -62,13 +64,44 @@ export default async function handler(
       Authorization: 'Token ' + process.env.REPLICATE_API_KEY,
     },
     body: JSON.stringify({
-      version:
-        '9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3',
-      input: { img: imageUrl, version: 'v1.4', scale: 2 },
+      version: 'a07f252abbbd832009640b27f063ea52d87d7a23a185ca165bec23b5adc8deaf',
+      input: {
+        image: imageUrl,
+        style: "Clay",
+        prompt: "a person in a post apocalyptic war game",
+        instant_id_strength: 0.8
+      },
     }),
   });
+  let requestBody = {
+    version: 'a07f252abbbd832009640b27f063ea52d87d7a23a185ca165bec23b5adc8deaf',
+    input: {
+      image: imageUrl, // 从请求体中获取的图像URL
+      style: "Clay",
+      prompt: "a person in a post apocalyptic war game",
+      lora_scale: 0.5,
+      custom_lora_url: "https://replicate.delivery/pbxt/[id]/trained_model.tar",
+      negative_prompt: "no ruins",
+      prompt_strength: 0.8,
+      denoising_strength: 0.1,
+      instant_id_strength: 0.8,
+      control_depth_strength: 0.3
+    }
+  };
+
 
   let jsonStartResponse = await startResponse.json();
+  if (jsonStartResponse.status === 'succeeded') {
+    let outputUrls = jsonStartResponse.output; // 假设输出是一个包含URI的数组
+    console.log("Generated Images:", outputUrls);
+    res.status(200).json(outputUrls);
+  } else if (jsonStartResponse.status === 'failed') {
+    console.log('Failed to generate images:', jsonStartResponse.error);
+    res.status(500).json('Failed to restore image');
+  } else {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // 如果状态不是成功也不是失败，可能还在处理中
+  }
+
   let endpointUrl = jsonStartResponse.urls.get;
 
   // GET request to get the status of the image restoration process & return the result when it's ready
--- utils/redis.ts
+++ utils/redis.ts
@@ -3,9 +3,9 @@ import { Redis } from "@upstash/redis";
 const redis =
   !!process.env.UPSTASH_REDIS_REST_URL && !!process.env.UPSTASH_REDIS_REST_TOKEN
     ? new Redis({
-        url: process.env.UPSTASH_REDIS_REST_URL,
-        token: process.env.UPSTASH_REDIS_REST_TOKEN,
-      })
+      url: process.env.UPSTASH_REDIS_REST_URL,
+      token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    })
     : undefined;
 
 export default redis;
